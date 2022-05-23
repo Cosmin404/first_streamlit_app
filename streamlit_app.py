aad99ca9a71cec5ed634e701602b425d8eb0af2f
Cosmin404 <85865015+Cosmin404@users.noreply.github.com>
2022-05-23 10:27:58 +0300

    add menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
-streamlit.title("My first streamlit app.")
+streamlit.title("My first streamlit breakfast")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
