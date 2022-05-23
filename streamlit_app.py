524bb9861709fd90cae6ee209f336777453ff37d
Cosmin404 <85865015+Cosmin404@users.noreply.github.com>
2022-05-23 10:19:39 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit
 streamlit.title("My first streamlit app.")
-from PIL import Image
-image = Image.open('sunrise.jpg')
 
+image = Image.open('https://www.google.com/url?sa=i&url=https%3A%2F%2Fid.usembassy.gov%2Fstudy-on-the-peoples-republic-of-chinas-south-china-sea-maritime-claims%2F&psig=AOvVaw1OQx6FX9zEPvTc4arokrM1&ust=1653376755187000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCKiz4sOK9fcCFQAAAAAdAAAAABAD')
 st.image(image, caption='Sunrise by the mountains')
