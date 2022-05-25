0cf9d92baae068abe5ff91a8e9596db72fa23276
Cosmin404 <85865015+Cosmin404@users.noreply.github.com>
2022-05-25 16:37:19 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,11 +24,11 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruitvice Fruit Advice')
-  try:
-    fruit_choice=streamlit.text_input('What fruit would you like information about?')
-    if not fruit_choice:
-      streamlit.error("Select a fruit to get information about it.")
-  else:
+try:
+  fruit_choice=streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+  streamlit.error("Select a fruit to get information about it.")
+else:
 fruitvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 fruitvice_normalized=pandas.json_normalize(fruitvice_response.json())
 streamlit.dataframe(fruitvice_normalized)
