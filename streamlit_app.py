448ba7cbfee260b50a6e99638f6664b868eeeab6
Cosmin404 <85865015+Cosmin404@users.noreply.github.com>
2022-05-25 16:59:15 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ fruits_selected=streamlit.multiselect("Pick some fruits:",list(my_fruit_list.ind
 fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-def get_fruitvice_data(this_fruit_choice)
+def get_fruitvice_data(this_fruit_choice):
     fruitvice_response=requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
     fruitvice_normalized=pandas.json_normalize(fruitvice_response.json())
     return fruitvice_normalized
