99063a045b039f4915a90df5b0bdef2865543bba
Cosmin404 <85865015+Cosmin404@users.noreply.github.com>
2022-05-25 16:27:20 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 streamlit.title("My first streamlit breakfast")
 
 streamlit.header('Breakfast Menu')
@@ -7,7 +12,7 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -21,13 +26,14 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruitvice Fruit Advice')
 fruit_choice=streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered',fruit_choice)
-import requests
+
 fruitvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruitvice_response.json())
 fruitvice_normalized=pandas.json_normalize(fruitvice_response.json())
 streamlit.dataframe(fruitvice_normalized)
+streamlit.stop()
 
-import snowflake.connector
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("Select * from fruit_load_list")
